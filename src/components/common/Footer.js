591ac077d60104851a6ef6689d9dbb9dd04210fd
jaymagrob <johnmagnusrobertson@gmail.com>
2020-02-03 14:09:25 +0000

    updated background

---
+++ src/components/common/Footer.js
@@ -0,0 +1,12 @@
+//Component used as footer
+//Component is not routed but displayed in app.js
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => (
+  <footer className="fix-footer">Footer</footer>
+
+)
+
+export default Footer
