8534ad35145c796102019b83229e0fafbcab8619
jaymagrob <johnmagnusrobertson@gmail.com>
2020-03-10 09:49:04 +0000

    Ready for deployment

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose')
 const app = express()
 const { port, dbURI } = require('./config/environment')
 
+
+app.use(express.static(`${__dirname}/dist`))
+
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.js`))
+
 mongoose.connect(dbURI)
-
-// app.use(express.static(`${__dirname}/dist`))
-
-// app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.js`))
 
 app.listen(port, () => console.log(`app is listening on port ${port}`))
